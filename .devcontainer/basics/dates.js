c7856ad99dc1b704d0286c81b3df12b1e491b97b
Vivek Kumar <vivekkumar16416@gmail.com>
2026-06-04 05:32:55 +0000

    basics completed

---
+++ .devcontainer/basics/dates.js
@@ -0,0 +1,16 @@
+// Date is a `object` in js 
+
+
+//console.log(Temporal.Now.plainDateTimeISO().toString()) // It will give us the current date and time in ISO format (2024-06-17T12:34:56.789Z) . It is a static method of Temporal object . It is a modern way of getting the current date and time . It is more accurate and reliable than the old way of using Date object .
+// NOT WORKING BRO !
+
+
+
+const date = new Date() // It will give us the current date and time in a human readable format (Mon Jun 17 2024 12:34:56 GMT+0000 (Coordinated Universal Time)) . It is the old way of getting the current date and time . It is less accurate and reliable than the modern way of using Temporal object .
+
+console.log(date) // It will print the current date and time in a human readable format .
+
+
+
+
+
