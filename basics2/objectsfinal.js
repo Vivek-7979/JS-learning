72779068f404c05f6d1c838a2d36a9ab89e3cfc0
Vivek Kumar <vivekkumar16416@gmail.com>
2026-06-05 04:55:50 +0000

    json,desconstructing and objects completed

--- basics2/objectsfinal.js
+++ basics2/objectsfinal.js
@@ -14,3 +14,19 @@ const { courseinstructor: inst} = course // from course object the name of cours
 console.log(inst)                        // Answer will be same
 
 
+// API returns the value in JSon format . 
+
+// JSON(javascript object notation) format = 
+// { "key": "value"}
+//   The only diff between the json and object is that the key is also written inside the string in JSON .
+
+// The API can also return the value in array format where the array contains many other objects inside it.
+//like:
+// [ {}, {}, {},{}]
+
+
+
+
+
+
+
