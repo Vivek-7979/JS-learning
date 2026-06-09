32a5cb62f63329747fa96f68a28e19dd75ae4f02
Vivek Kumar <vivekkumar16416@gmail.com>
2026-06-09 17:32:20 +0000

    IIFE function

---
+++ basics3/IIFE.js
@@ -0,0 +1,20 @@
+// Immediately Invoked Function Expressions ( IIFE )      : MATLAB JIWE HI bnaya - ohve hi execute ho jawe . 
+
+/*      global scope ke variables de karan pollution ho janda kei baar so ohi pollution nu hatane de vaste we use the the IIFE function .    */
+
+
+// It has many applications like : 1.] jiwe hi mein apni website on kara jade ohi database connect ho jawe..
+
+function simpleFunction  ()  {} 
+simpleFunction()                  // we called it 
+
+
+
+// ++++++++++++++++++++++++++++++  Syntax IIFE : (function / arrow function )()
+
+
+
+(function IIFE ( ) {} ) ()   // Naale bnake naal hi call karta
+
+// two types of IIFE : 1.) named IIFE(function has name)    2.)UnNamed IIFE ( function hasnot have name )
+
