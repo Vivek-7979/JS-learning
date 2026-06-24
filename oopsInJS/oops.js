5a0e36906febc0dfa011cf006326f872e54fcfdd
Vivek-7979 <vivekkumar16416@gmail.com>
2026-06-24 17:01:07 +0530

    prototype

--- oopsInJS/oops.js
+++ oopsInJS/oops.js
@@ -27,3 +27,19 @@ console.log(user.method1());     // accessing the method inside the object
                           
 // new = constructor function ( to create a new context/space for that item ( object,function etc.)  )
 //       when we write the new it calls a constructor function . That automatically creates a empty object for that item ( can be object , function , variable , promise etc)  .  { New = independent item hai eh }  
+
+
+// this = jisne bhi bulaya usda kaam krdo ; kisda kaam karna oh dsda this ( isda )
+
+
+//  JS has " prototyple behaviour "  => matlab js all elements (every element is object in js) are interlinked with each other through a chain of linked prototype ... { enaling inheritance and memory efficiency} .. { je kuch na mile js nu upar oth upar jayie jana . parent -> grandparent -> further...}
+
+// like :-            Array    ->      Object   -> null
+//                                 /
+//                                /       ^
+//                    function   /        |
+//                                      String
+
+/*  Every elements in js is technically object . like - they are pointing to the object (parent) . So , they also gets the property of object             */
+
+
