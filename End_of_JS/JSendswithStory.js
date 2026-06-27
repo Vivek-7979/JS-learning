54359055b6ff18128929628eeab1f2fa81fa978b
Vivek-7979 <vivekkumar16416@gmail.com>
2026-06-27 11:11:39 +0530

    Js Ended officialy - And it Ends with a Story

---
+++ End_of_JS/JSendswithStory.js
@@ -0,0 +1,23 @@
+//      JS ENDS WITH A STORY 
+
+/* 
+
+                                         " THE CORE JS ENDS HERE ...."
+   
+:- kichne nu taah jihna marji kichi jao is js nu . But , oh jehre topics hounga oh 
+   kisi sub-topic de bhi sub parts hona .. Exceptions and rare case { eh teh vse bhi documentation parh ke a hi jana - ihna teh kabil ho hi gye ve }
+   Because , js de andar jo main-main jo 'compulsory' c oh teh sara hi sikh leya vaa .... ENOUGH |||
+   
+   ** HUN baki , teh taah jehre chote-chote point hai in depth (oh karke hi aune jado apa real - world project krna).  Bcz , oh kisi bhi video/paid course ch hone hi nhi .
+      bcz , oh scenarios neh jado apa fasna kite ya project banan tah pta lagna . Is karke oh kisi paid course ch ho bhi nhi skde na hi hunde ....   JO KITA NA HUN EH BHOTT AWWW | 
+
+      : Interview ch bhi puchde ge chlo oh chote-chote point . But , Oh point real-world ch kaam krke pta lagne ... 
+      
+      { HUN jo kaam krleya va na oh more than enough ga js complete krn nu , kisi bhi course ya paid course ch ihna depth , ihna zada , ya inaa vdiya ds hi nhi sakde - possible hi nhi haii }
+      { Summary : I learnt from bestest on Internt - Now , I am Ready To GO !}
+      
+      
+                                        OFFICIALLY THE JS ENDS HERE    !!!!!                                                */
+      
+      
+                                                                         
